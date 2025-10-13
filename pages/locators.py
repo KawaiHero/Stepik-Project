dfc760dc052a9582619977f09624b2e78060cd4e
Talgat <gumirovt@mail.ru>
2025-10-13 22:07:38 +0200

    add steps from 4.3 lesson

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    PRODUCT_BUTTON = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '[class="price_color"]')
+    BUCKET_PRICE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    PRODUCT_NAME_IN_BUCKET = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+import time
+
+from .base_page import BasePage, solve_quiz_and_get_code
+from .locators import ProductPageLocators
+
+def normalize_price(text: str) -> str:
+    text = text.replace('\xa0', ' ').strip()
+    if text.startswith('£'):
+        text = text[1:].strip() + ' £'
+    return text
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        product_button = self.browser.find_element(*ProductPageLocators.PRODUCT_BUTTON)
+        product_button.click()
+        solve_quiz_and_get_code(self)
+        time.sleep(2)
+
+    def should_be_same_price(self):
+        bucket_price = self.browser.find_element(*ProductPageLocators.BUCKET_PRICE).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        bucket_price = normalize_price(bucket_price)
+        product_price = normalize_price(product_price)
+        assert product_price == bucket_price, "The product price is incorrect"
+
+    def should_be_same_pruduct_name(self):
+        product_name_in_bucket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BUCKET).text
+        product_name_on_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert product_name_in_bucket == product_name_on_page, "The product name is incorrect"
